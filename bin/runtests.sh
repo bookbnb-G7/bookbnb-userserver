25f3c630d942bef8ff5322899c3e4f325250e5ac
facutorraca <ftorraca@fi.uba.ar>
2020-10-18 21:57:13 -0300

    add check for database connection

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-docker-compose up -d
+docker-compose up -d 
+docker exec bookbnb-userserver_web bash -c 'while !</dev/tcp/db/5432; do sleep 1; done;'
 docker exec bookbnb-userserver_web npm test
 docker-compose down
