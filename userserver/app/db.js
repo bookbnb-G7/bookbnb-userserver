2efb498306c91cf8a05f6967af6ebdf34bb3f6dc
komod0 <juliancrespo15@gmail.com>
2020-10-22 04:19:10 -0300

    Test eslint

--- userserver/app/db.js
+++ userserver/app/db.js
@@ -1,14 +1,15 @@
 const Sequelize = require('sequelize')
 require('dotenv').config()
 
+
 const DATABASE_URL = process.env.DATABASE_URL
 
 console.log(DATABASE_URL)
 
 const database = new Sequelize(DATABASE_URL ,{
-  	database: 'users',
-  	dialect: 'postgres',
-  	operatorsAliases: Sequelize.Op
+  database: 'users',
+  dialect: 'postgres',
+  operatorsAliases: Sequelize.Op
 })
 
 module.exports = database 
--- userserver/app/main.js
+++ userserver/app/main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 
+
 require('dotenv').config()
 
 const database = require('./db')
@@ -14,13 +15,13 @@ app.use(cors())
 app.use(bodyParser.json())
 
 app.get('/', (req, res) => {
-	res.json({message:'userserver'})
+    res.json({message:'userserver'})
 })
 
 app.use('/users', require('./api/routes/user.router'))
 
 database.sync().then(() => {
- 	app.listen(port, () => {
-    	console.log(`Listening on port ${port}`)
-  	})
+    app.listen(port, () => {
+        console.log(`Listening on port ${port}`)
+    })
 })
