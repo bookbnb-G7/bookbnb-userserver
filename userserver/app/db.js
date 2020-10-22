c26776026913163894a936765b6f1f66caae792c
komod0 <juliancrespo15@gmail.com>
2020-10-22 04:22:03 -0300

    Add eslint

--- userserver/app/db.js
+++ userserver/app/db.js
@@ -7,9 +7,9 @@ const DATABASE_URL = process.env.DATABASE_URL
 console.log(DATABASE_URL)
 
 const database = new Sequelize(DATABASE_URL ,{
-  database: 'users',
-  dialect: 'postgres',
-  operatorsAliases: Sequelize.Op
+    database: 'users',
+    dialect: 'postgres',
+    operatorsAliases: Sequelize.Op
 })
 
 module.exports = database 
