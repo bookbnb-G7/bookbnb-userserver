f88ac21289d4a22ac7a1b04c896dd9e9b75cfca7
nicofandos <nfandos@fi.uba.ar>
2020-11-02 02:27:54 -0300

    Sqlite agregado para test, el archivo no se borra solo despues de test

--- userserver/app/db.js
+++ userserver/app/db.js
@@ -1,14 +1,27 @@
-const Sequelize = require('sequelize')
-require('dotenv').config()
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const DATABASE_URL = process.env.DATABASE_URL
+const DATABASE_URL = process.env.DATABASE_URL;
+const ENVIRONMENT = process.env.ENVIRONMENT;
 
 console.log(DATABASE_URL)
 
-const database = new Sequelize(DATABASE_URL ,{
-  	dialect: 'postgres',
-  	operatorsAliases: Sequelize.Op,
-    define: {timestamp: false}
-})
+let database = null;
+
+if (ENVIRONMENT == 'production') {
+	database = new Sequelize(DATABASE_URL ,{
+		dialect: 'postgres',
+		operatorsAliases: Sequelize.Op,
+	  	define: {timestamp: false}
+  	})
+} else {
+	database = new Sequelize(DATABASE_URL ,{
+		dialect: 'sqlite',
+		operatorsAliases: Sequelize.Op,
+	  	define: {timestamp: false},
+		storage: './database.sqlite3'
+  	})
+}
+
 
 module.exports = database 
