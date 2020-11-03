e575cdf35c410efe7966951941db38a15c3d3c18
facutorraca <ftorraca@fi.uba.ar>
2020-11-03 07:23:38 -0300

    fix testing database

--- userserver/app/db.js
+++ userserver/app/db.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
+const TESTING_DB_URL = 'sqlite:///./test.db'
 const DATABASE_URL = process.env.DATABASE_URL;
 const ENVIRONMENT = process.env.ENVIRONMENT;
 
@@ -12,14 +13,26 @@ if (ENVIRONMENT == 'production') {
 	database = new Sequelize(DATABASE_URL ,{
 		dialect: 'postgres',
 		operatorsAliases: Sequelize.Op,
-	  	define: {timestamp: false}
-  	})
-} else {
+	  define: {timestamp: false}
+  })
+} 
+
+if (ENVIRONMENT == 'development') {
 	database = new Sequelize(DATABASE_URL ,{
 		dialect: 'sqlite',
 		operatorsAliases: Sequelize.Op,
+		define: {timestamp: false},
+		storage: './app.db'
+  })
+}
+
+
+if (ENVIRONMENT == 'testing') {
+	database = new Sequelize(TESTING_DB_URL ,{
+		dialect: 'sqlite',
+		operatorsAliases: Sequelize.Op,
 		define: {timestamp: false}
-  	})
+  })
 }
 
 module.exports = database 
